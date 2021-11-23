3036027f36588dbb3b3bbd9bfb1811dd78fbd009
Rodrigo Amaral <rodrigo.com2@gmail.com>
2021-11-23 19:39:21 -0300

    Iniciando o projeto

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("*********************************************************\n")
+	fmt.Printf("**                                                     **\n")
+	fmt.Printf("**                 PROGRAMADOR CP2102N                 **\n")
+	fmt.Printf("**                                                     **\n")
+	fmt.Printf("*********************************************************\n")
+
+}
